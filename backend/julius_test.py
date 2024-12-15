c526188e33bb23386002ca1307c76a597c97715d
hm-ob <144942205+hm-ob@users.noreply.github.com>
2024-12-15 15:53:05 +0900

    Update julius_test.py

--- backend/julius_test.py
+++ backend/julius_test.py
@@ -4,12 +4,14 @@ import sys
 import os
 import julius
 
+#パス設定
 julius_path = "C:/cygwin64/home/Hoshi_Manami/Python/project/dictation-kit-4.5/bin/windows/julius.exe"
 main = "C:/cygwin64/home/Hoshi_Manami/Python/project/dictation-kit-4.5/main.jconf"
 am_dnn = "C:/cygwin64/home/Hoshi_Manami/Python/project/dictation-kit-4.5/am-dnn.jconf"
 julius_dnn = "C:/cygwin64/home/Hoshi_Manami/Python/project/dictation-kit-4.5/julius.dnnconf"
 input_file = "C:/cygwin64/home/Hoshi_Manami/Python/project/dictation-kit-4.5/BASIC5000_0001.wav"
 
+#インスタント化、関数呼び出し
 reco = julius.Julius_Recognition(julius_path, main, am_dnn, julius_dnn, input_file)
 result = reco.recognition()
-print(result)
+print(result) #認識結果出力
