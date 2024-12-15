5db64af492ab226937fc78afbfbbe3e5cc706619
Riho0 <misaki04141910035@gmail.com>
2024-12-15 15:44:45 +0900

    docker flask

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def flask_app():
-    return 'おめでとう成功だ!!'
-
+    return 'おめでとう成功だ!!!'
+def hello():
+    name = "who"
+    #return name
+    return render_template('hello.html', title='hello2', name=name)
 
 if __name__ == "__main__":
     app.run(debug=True, port=5001, threaded=True)  
