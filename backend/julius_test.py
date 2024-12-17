f6777d20894eb2546d096325da760990e5432a40
Riho0 <misaki04141910035@gmail.com>
2024-12-17 15:28:54 +0900

    index

--- backend/julius_test.py
+++ backend/julius_test.py
@@ -3,17 +3,13 @@ import re
 import sys
 import os
 import julius
-ain = "/app/dictation-kit-4.5/main.jconf"
-am_dnn = "/app/dictation-kit-4.5/am-dnn.jconf"
-julius_dnn = "/app/dictation-kit-4.5/julius.dnnconf"
-input_file = "/app/dictation-kit-4.5/BASIC5000_0001.wav"
-julius_path = "/app/dictation-kit-4.5/bin/window/julius.exe"
+
 #相対パス設定
-main = "/app/dictation-kit-4.5/main.jconf"
-am_dnn = "/app/dictation-kit-4.5/am-dnn.jconf"
-julius_dnn = "/app/dictation-kit-4.5/julius.dnnconf"
-input_file = "/app/dictation-kit-4.5/BASIC5000_0001.wav"
-julius_path = "/app/dictation-kit-4.5/bin/window/julius.exe"
+main = "C:/Users/misak/proj_2/backend/dictation-kit-4.5/main.jconf"
+am_dnn = "C:/Users/misak/proj_2/backend/dictation-kit-4.5/am-dnn.jconf"
+julius_dnn = "C:/Users/misak/proj_2/backend/dictation-kit-4.5/julius.dnnconf"
+input_file = "C:/Users/misak/proj_2/backend/dictation-kit-4.5/BASIC5000_0001.wav"
+julius_path = "C:/Users/misak/proj_2/backend/dictation-kit-4.5/bin/window/julius.exe"
 #"C:\Users\misak\proj_2\backend\dictation-kit-4.5\bin\windows\julius.exe"
 
 #インスタント化、関数呼び出し
@@ -21,7 +17,6 @@ def sentence():
     reco = julius.Julius_Recognition(julius_path, main, am_dnn, julius_dnn, input_file)
     result = reco.recognition()
     print(result) #認識結果出力
-    
     return result
 def main():
     sentence()
